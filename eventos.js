de79ba0799879a4ee1e1b85bfb741f450d36f04f
Sof <sofiarameau02@gmail.com>
2024-08-13 23:08:09 -0300

    solucionado el problema de propagacion de clicks en el boton y el div

--- eventos.js
+++ eventos.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const div = document.getElementById('miDiv');
+    const boton = document.getElementById('miBoton');
+
     div.addEventListener('click', () => {
         alert('Hola! Soy el div');
+    });
+
+    boton.addEventListener('click', (event) => {
+        alert('Hola!');
+        event.stopPropagation();
     });
 });
